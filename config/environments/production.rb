14305a039338078a814d7275ef3be50d48ca4e39
Michael Lacy <michael.lacy@gmail.com>
2014-01-31 14:57:48 -0500

    cdn configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ SubscriptionService::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "cdn.season.to"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
@@ -78,7 +78,7 @@ SubscriptionService::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'season-to.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'season.to' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -99,7 +99,7 @@ SubscriptionService::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'neurokinetikz_development',
+      :bucket => 'season_to_development',
       :access_key_id => '1H19MRTT3YVYAZQYBBG2',
       :secret_access_key => 'gVXBOURFujZmyLI87retOYbCzY1fshKeNRTkSLno'
     }
