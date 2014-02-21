60e2b415fac9393bd9345e6a6662f735c4acc442
Michael Lacy <michael.lacy@gmail.com>
2014-02-21 10:03:08 -0500

    braintree webhook

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,10 +1,4 @@
 class BraintreeController < ApplicationController
-  def challenge
-  	challenge = request.params["bt_challenge"]
-    challenge_response = Braintree::WebhookNotification.verify(challenge)
-    return [200, challenge_response]
-  end
-
   def webhook
   	webhook_notification = Braintree::WebhookNotification.parse(request.params["bt_signature"], request.params["bt_payload"])
     puts "[Webhook Received #{webhook_notification.timestamp}] Kind: #{webhook_notification.kind} | Subscription: #{webhook_notification.subscription.id}"
