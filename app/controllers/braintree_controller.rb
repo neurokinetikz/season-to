57fee1a1b913e096372447edbcb7408c86fb67d9
Michael Lacy <michael.lacy@gmail.com>
2014-02-21 11:57:42 -0500

    log webhook

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -8,6 +8,21 @@ class BraintreeController < ApplicationController
   def webhook
   	webhook_notification = Braintree::WebhookNotification.parse(params[:bt_signature], params[:bt_payload])
     puts "[Webhook Received #{webhook_notification.timestamp}] Kind: #{webhook_notification.kind} | Subscription: #{webhook_notification.subscription.id}"
+    puts "#{webhook_notification}"
+
+    case webhook_notification.kind
+    when 'subscription_charged_successfully'
+    when 'subscription_charged_unsuccessfully'
+    when 'subscription_went_active'
+    when 'subscription_went_past_due'
+    when 'subscription_canceled'
+    when 'subscription_expired'
+    else
+    end
+    
     render nothing: true, status: 200
   end
 end
+
+signature, payload = Braintree::WebhookTesting.sample_notification(Braintree::WebhookNotification::Kind::SubscriptionChargedSuccessfully, 'c88ysr')
+y Braintree::WebhookNotification.parse(signature, payload)
